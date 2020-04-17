38f040b35092ff9af706dd58f7af28bb828637cd
Adam Johnson <johnsonadam187@gmail.com>
2020-04-17 22:04:45 +1200

    re-factor of initial mapping project, into pycharm

---
+++ Covid_map_nz/covid_map_nz.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+
